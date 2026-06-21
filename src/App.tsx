9481219639025d35f8db5d41152b343c82bf02a2
Justin Ye <justinye787@gmail.com>
2026-06-20 22:25:47 -0400

    moving things

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Hello, World! Welcome to my portfolio.</h1>
+      <p>This is where the magic happens.</p>
+    </div>
+  )
+}
+
+export default App
