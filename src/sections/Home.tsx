7bde5d5a9cde0c2ef7625764b229ba8d1951969c
Justin Ye <justinye787@gmail.com>
2026-06-21 16:40:06 -0400

    Finished home page large text

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,8 +1,8 @@
 function Home() {
   return (
-    <section id="home" style={{ padding: '4rem 2rem' }}>
-      <h2>Home Page</h2>
-      <p>This is the home page!</p>
+    <section id="home" style={{ padding: '4rem 3rem' }}>
+      <h1>Hi, I'm Justin!</h1>
+      <h2>I'm a software engineer <span style={{ display: 'block' }}></span>who's interested in full-stack development</h2>
     </section>
   );
 }
