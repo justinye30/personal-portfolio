b27453e97c95311bb6f9ec63d23d066636f0f1f6
Justin Ye <justinye787@gmail.com>
2026-07-05 22:44:37 -0400

    Added stars to home page

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,8 +1,30 @@
+import { useEffect } from 'react';
+import starImg from '../assets/star.png';
+
 function Home() {
+  useEffect(() => {
+    document.body.classList.add('home-page-body');
+
+    return () => {
+      document.body.classList.remove('home-page-body');
+    };
+  }, []);
+
   return (
-    <section id="home" style={{ padding: '6rem 1.5rem' }}>
-      <h1>Hi, I'm Justin!</h1>
-      <h2>I'm a software engineer <span style={{ display: 'block' }}></span>who's interested in full-stack development</h2>
+    <section id="home" className="home-container">
+      <div className="home-content">
+        <h1>Hi, I'm Justin!</h1>
+        <h2>
+          I'm a software engineer who's interested in full-stack development
+        </h2>
+      </div>
+
+      <div className="home-stars-bg">
+        <img src={starImg} alt="" className="bg-star star-giant" />
+        <img src={starImg} alt="" className="bg-star star-small-top" />
+        <img src={starImg} alt="" className="bg-star star-small-right" />
+        <img src={starImg} alt="" className="bg-star star-small-bottom" />
+      </div>
     </section>
   );
 }
