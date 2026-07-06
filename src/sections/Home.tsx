96850444a5614c2f9bc5d6bed70989403816de63
Justin Ye <justinye787@gmail.com>
2026-07-05 23:24:42 -0400

    fixed text shadow things

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -13,10 +13,12 @@ function Home() {
   return (
     <section id="home" className="home-container">
       <div className="home-content">
-        <h1>Hi, I'm Justin!</h1>
-        <h2>
-          I'm a software engineer who's interested in full-stack development
-        </h2>
+        <div className="home-title">
+          <h1>Hi, I'm Justin!</h1>
+        </div>
+        <div className="home-text">
+          <h2>I'm a software engineer who's interested in full-stack development</h2>
+        </div>
       </div>
 
       <div className="home-stars-bg">
